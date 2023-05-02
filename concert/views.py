30793f84df449c17775b10d8c4f108cde5a72185
Juan Alvarado <jao_ec@yahoo.com>
2023-05-02 23:00:54 +0200

    Update views.py

--- concert/views.py
+++ concert/views.py
@@ -33,32 +33,16 @@ def signup(request):
 def index(request):
     return render(request, "index.html")
 
-
-#def songs(request):
-    # songs = {"songs":[]}
-    # return render(request, "songs.html", {"songs": [insert list here]})
-    # pass
+SONGS_URL = "https://songs-sn-labs-jaoxxx3.labs-prod-openshift-san-a45631dc5778dc6371c67d206ba9ae5c-0000.us-east.containers.appdomain.cloud"
+PHOTO_URL = "https://pictures.11zdvgafopn2.us-south.codeengine.appdomain.cloud"
 
 def songs(request):
-    songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
-    return render(request, "songs.html", {"songs":songs["songs"]})
-
-#def photos(request):
-    # photos = []
-    # return render(request, "photos.html", {"photos": photos})
-    #pass
+    songs = req.get(f"{SONGS_URL}/song").json()
+    return render(request, "songs.html", {"songs": songs["songs"]})
 
 def photos(request):
-    photos = [{
-    "id": 1,
-    "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
-    "event_country": "United States",
-    "event_state": "District of Columbia",
-    "event_city": "Washington",
-    "event_date": "11/16/2022"
-    }]
+    photos = req.get(f"{PHOTO_URL}/picture").json()
     return render(request, "photos.html", {"photos": photos})
-
 
 def login_view(request):
     if request.method == "POST":
